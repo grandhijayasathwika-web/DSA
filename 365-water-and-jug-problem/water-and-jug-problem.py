a370bc2f56ee19ac0140d0ae7e57227269abcc87
Grandhi jaya sathwika <grandhijayasathwika@gmail.com>
2026-06-15 15:16:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (3.57%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.py
+++ 365-water-and-jug-problem/water-and-jug-problem.py
@@ -1,11 +1,15 @@
 class Solution(object):
-   def canMeasureWater(self, jug1Capacity, jug2Capacity, targetCapacity):
-        x = self.gcd(jug1Capacity, jug2Capacity)
-        if jug1Capacity + jug2Capacity < targetCapacity:
-            return False
-        return targetCapacity % x == 0
-
-   def gcd(self, a, b):
-        while b != 0:
-            a, b = b, a % b
-        return a
+    def canMeasureWater(self, x, y, target):
+        """
+        :type x: int
+        :type y: int
+        :type target: int
+        :rtype: bool
+        """
+        def gcd(a,b):
+            while b!=0:
+                a,b=b,a%b
+            return a
+        a=gcd(x,y)
+        if x+y<target:return False
+        return target%a==0

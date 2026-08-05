0faea8fd2f2eacca5fbae691c8be31c017482be1
Grandhi jaya sathwika <grandhijayasathwika@gmail.com>
2026-08-05 18:46:47 +0530

    Time: 474 ms (85.71%) | Memory: 91 MB (71.43%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -0,0 +1,34 @@
+from collections import deque
+class Solution(object):
+    def remainingMethods(self, n, k, invocations):
+        """
+        :type n: int
+        :type k: int
+        :type invocations: List[List[int]]
+        :rtype: List[int]
+        """
+        edges=[[] for _ in range(n)]
+        in_degree=[0]*n
+        for u,v in invocations:
+            edges[u].append(v)
+            in_degree[v]+=1
+        queue=deque([k])
+        suspicious=[0]*(n)
+        suspicious[k]=1
+        while queue:
+            u=queue.popleft()
+            for v in edges[u]:
+                in_degree[v]-=1
+                if suspicious[v]==0:
+                    queue.append(v)
+                    suspicious[v]=1
+        can_remove_all=True
+        for i in range(n):
+            if suspicious[i]==1 and in_degree[i]>0:
+                can_remove_all=False
+                break
+        if not can_remove_all:
+            return list(range(n))
+        return [i for i in range(n) if suspicious[i]==0]
+
+        
